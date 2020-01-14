5dbf9b2e9a2e5c49646a956b1be89aa1b8dad30b
YousefBouland <37432025+YousefBouland@users.noreply.github.com>
2020-01-14 12:12:01 -0800

    Add files via upload

---
+++ homework8/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ResultsComponent } from './results/results.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+
+const routes: Routes = [
+  {path: 'results', component: ResultsComponent},
+  {path: 'favorites', component: FavoritesComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
